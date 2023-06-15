6c7a5aa17287e5a6d35cbbd831d17f5ef63ba347
Sara <saraprus@elfak.rs>
2023-06-15 12:09:18 +0200

    merge

--- Cudovista3/Controllers/BajaliceControllers.cs
+++ Cudovista3/Controllers/BajaliceControllers.cs
@@ -11,27 +11,10 @@ namespace Cudovista3.Controllers
     [ApiController]
     public class BajaliceControllers : ControllerBase
     {
-         
 
-        [HttpPost]
-        [Route("DodajBajalicu/{cudovisteID}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult AddBajalica([FromRoute(Name = "cudovisteID")] int cudovisteID, [FromBody] BajaliceView o)
-        {
-            try
-            {
-                // var cudoviste = DataProvajderS.VratiMagijskoCudoviste(cudovisteID);
-
-                DataProvajderS.SacuvajBajalicu(o, cudovisteID);
-
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.ToString());
-            }
-        }
         
+
+
+
     }
 }
